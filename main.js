38b5a3622f19b795ffae6a76fd46d73447af60c4
Juno-Cheng <chengjonathan2021@gmail.com>
2023-10-02 22:47:07 -0700

    Snapshot

---
+++ main.js
@@ -0,0 +1,23 @@
+class Player{
+    constructor(characterNane = "Naruto"){
+        this.name = characterNane;
+        this.attributes = {
+            strength: 0,
+            dexterity: 0,
+            constitution: 0,
+            intelligence: 0,
+            wisdom: 0,
+            charisma: 0
+        };
+    }
+
+    rollAttribute(){
+        console.log("Rolling Dice...");
+    }
+
+    printPlayer()
+
+
+
+
+}
